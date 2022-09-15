0a27a1b557f5a53bac8fa3b4e0d91d8700e718fa
Kakeru Sato <sato.kakeru3@gmail.com>
2022-09-15 14:03:21 +0900

    pre

--- js/component/directory.js
+++ js/component/directory.js
@@ -12,6 +12,7 @@ class Directory {
         this.classNameItem = 'item';
         this.classNameDropDown = 'drop-down';
         this.classNameActive = 'active';
+        this.classNameList = 'list';
         this.classNameSubList = 'sub-list';
     }
 
@@ -30,9 +31,36 @@ class Directory {
     setUpForDropDown() {
         const elms = document.querySelectorAll(`.${this.classNameDirectory} .${this.classNameItem}.${this.classNameDropDown}`);
         for(let i = 0; i < elms.length; i++) {
-            elms[i].addEventListener('click', () => {
-                elms[i].classList.toggle(this.classNameActive);
+            const elm = elms[i];
+            this.setUpMaxHeight(elm);
+            elm.addEventListener('click', () => {
+                elm.classList.toggle(this.classNameActive);
+                this.setUpMaxHeight(elm);
             });
+        }
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Set up max-height for sub-list
+    //////////////////////////////////////////////////////////////////////
+    async setUpMaxHeight(elm) {
+        const nextElm = elm.nextElementSibling; 
+        if(nextElm === undefined || nextElm === null) {
+            return;
+        }
+        const listElm = nextElm.querySelector(`.${this.classNameList}`);
+        if(listElm === undefined || listElm === null) {
+            return;
+        }
+        const listElmRect = listElm.getBoundingClientRect();
+        if(elm.classList.contains(this.classNameActive)) {
+            nextElm.style.maxHeight = `${listElmRect.height}px`;
+        }
+        if(elm.classList.contains(this.classNameActive)) {
+            nextElm.style.maxHeight = `${listElmRect.height}px`;
+        } else {
+            nextElm.style.maxHeight = 0;
         }
     }
 
